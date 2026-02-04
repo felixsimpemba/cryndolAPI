7a87855bdce61312e87ca9b52daa4d69a089ac5a
felixsimpemba <felixsimpemba6@gmail.com>
2026-02-04 19:43:57 +0200

    ese

--- app/Http/Controllers/ApiDocController.php
+++ app/Http/Controllers/ApiDocController.php
@@ -5,5 +5,5 @@ namespace App\Http\Controllers;
 class ApiDocController extends Controller
 {
     // Deprecated: Top-level OpenAPI annotations have been consolidated into App\OpenApi\Docs.
-    // This class intentionally contains no OpenAPI annotations to avoid duplicates in Swagger UI.
+    // This class intentionally contains no OpenAPI annotations to avoid duplicates in Swagger UI. dd
 }
